f64758c325527d7f93195a1099ed65f62ab2c9de
Nome do Usuário <email_do.usuario@servidor.com>
2022-06-21 13:25:52 -0300

    Atualização

---
+++ index.js
@@ -0,0 +1,27 @@
+function somarDoisNumeros(numero1, numero2){
+    let soma = numero1 + numero2;
+    return('A soma dos dois numeros é: ' + soma)
+}
+
+function subtracaoEntreDoisNumeros(numero1, numero2){
+    let subtracao = numero1 - numero2;
+    return('A subtracao do primeiro pelo segundo número é: ' + subtracao)
+}
+
+function multiplicacaoDeDoisNumeros(numero1, numero2){
+    let multiplicacao = numero1 * numero2;
+    return('A multiplicação dos dois números é: ' + multiplicacao)
+}
+
+
+function divisaoEntreDoisNumeros(numero1, numero2){
+    let divisao = numero1 / numero2;
+    return('A divisão do primeiro pelo segundo número é: ' + divisao)
+}
+
+
+console.log(somarDoisNumeros(12 , 4))
+console.log(subtracaoEntreDoisNumeros(12 , 4))
+console.log(multiplicacaoDeDoisNumeros(12 , 4))
+console.log(divisaoEntreDoisNumeros(12 , 4))
+
